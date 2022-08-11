64d1dde035ed803cd95c0e74a5780cf54088bd46
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2022-08-11 21:41:43 +0200

    fix coverage, add coveralls to gulp

--- gulpfile.js/helpers.js
+++ gulpfile.js/helpers.js
@@ -1,8 +1,11 @@
 
 const { spawn } = require('child_process');
-function executeProcess(executable, parameters, directory = '.', stdio = 'inherit') {
+function executeProcess(executable, parameters, directory = '.', stdio = 'inherit', inputStream = null) {
 	const promise = new Promise((resolve, reject) => {
 		const process = spawn(executable, parameters, { stdio: stdio, cwd: directory });
+		if (inputStream !== null) {
+			inputStream.pipe(process.stdin);
+		}
 		process.on('close', (status) => {
 			if (status === 0) {
 				resolve(status);
--- gulpfile.js/test.js
+++ gulpfile.js/test.js
@@ -12,22 +12,25 @@ async function runTests() {
 
 async function fixJUnitTestReportPaths() {
 	let xmlContentBuffer = await fse.readFile('./tests/report-results/results.xml', 'utf-8');
-	xmlContentBuffer = xmlContentBuffer.replace(/file="[A-Za-z0-9\\/\\\\:_.\\-]*\/OSEventListener\/([A-Za-z0-9\\/\\\\:_.\\-]+)\.spec\.ts"/g, 'file="$1.spec.ts"');
+	xmlContentBuffer = xmlContentBuffer.replace(/file="[A-Za-z0-9\\/:_.\\-]*\/OSEventListener\/([A-Za-z0-9\\/_.\\-]+)\.spec\.ts"/g, 'file="$1.spec.ts"');
 	await fse.writeFile('./tests/report-results/results.xml', xmlContentBuffer);
 }
 
 async function fixMochaAwesomePaths() {
 	let jsonContentBuffer = await fse.readFile('./tests/report-results/mochawesome.json', 'utf-8');
-	jsonContentBuffer = jsonContentBuffer.replace(/"fullFile": "[A-Za-z0-9\\/\\\\:_.\\-]+\/OSEventListener\/([A-Za-z0-9\\/\\\\:_.\\-]+)\.spec\.ts"/g, '"fullFile": "$1.spec.ts"');
+	jsonContentBuffer = jsonContentBuffer.replace(/"fullFile": "[A-Za-z0-9\\/:_.\\-]+\/OSEventListener\/([A-Za-z0-9\\/_.\\-]+)\.spec\.ts"/g, '"fullFile": "$1.spec.ts"');
 	await fse.writeFile('./tests/report-results/mochawesome.json', jsonContentBuffer);
 	let htmlContentBuffer = await fse.readFile('./tests/report-results/mochawesome.html', 'utf-8');
-	htmlContentBuffer = htmlContentBuffer.replace(/&quot;(fullFile|jsonFile|htmlFile)&quot;:&quot;[A-Za-z0-9\\/\\\\:_.\\-]+\/OSEventListener\/([A-Za-z0-9\\/\\\\:_.\\-]+)(\.spec\.ts|\.json|\.html)&quot;/g, '&quot;$1&quot;:&quot;$2$3&quot;');
+	htmlContentBuffer = htmlContentBuffer.replace(/&quot;(fullFile|jsonFile|htmlFile)&quot;:&quot;[A-Za-z0-9\\/:_.\\-]+\/OSEventListener\/([A-Za-z0-9\\/_.\\-]+)(\.spec\.ts|\.json|\.html)&quot;/g, '&quot;$1&quot;:&quot;$2$3&quot;');
 	await fse.writeFile('./tests/report-results/mochawesome.html', htmlContentBuffer);
 }
 
+
 async function runCoverage() {
 	await executeProcess('npx', ['nyc', 'npm', 'run', 'test']);
-	//await executeProcess('npx', ['nyc', 'report', '--reporter=text-lcov');
+	await fse.remove('tests/report-results/tmpc');
+	const lcovStream = fse.createReadStream('tests/report-results/coverage/lcov.info');
+	await executeProcess('npx', ['coveralls'], '.', ['pipe', 'inherit', 'inherit'], lcovStream);
 }
 
 exports.startSampleServer = startSampleServer;
