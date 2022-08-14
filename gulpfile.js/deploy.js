49a14197bbe76489d8a27f72a8b14b10542318ea
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2022-08-14 14:19:45 +0200

    add deploy script to ci

--- gulpfile.js/deploy.js
+++ gulpfile.js/deploy.js
@@ -1,6 +1,7 @@
 const fse = require('fs-extra');
 const semver = require('semver');
-const pkgstat = require('pkgstat');
+const pinfo = require('package-info');
+const { executeProcess } = require('./helpers');
 
 async function getCurrentPackageVersion() {
 	const packageJSON = await fse.readJSON('package.json');
@@ -10,14 +11,21 @@ async function getCurrentPackageVersion() {
 }
 
 async function getDeployedPackageVersion() {
-	const packageInfo = await pkgstat('oseventlistener');
+	const packageInfo = await pinfo('oseventlistener');
 	const versionString = packageInfo.version;
+	const version = semver.parse(versionString);
+	return version;
 }
 
 async function publish() {
 	const currentVersion = await getCurrentPackageVersion();
 	const deployedVersion = await getDeployedPackageVersion();
-	console.log(currentVersion, deployedVersion);
+	if (semver.gte(deployedVersion, currentVersion)) {
+		throw Error(`currentVersion ${currentVersion.raw} <= deployedVersion ${deployedVersion}`);
+	}
+	const npmToken = process.env.NPM_TOKEN;
+	console.log(npmToken);
+	await executeProcess('npm', [ 'pack' ]);
 }
 
 exports.publish = publish;
