d1fcd95ba7b9840382f4640aa307e076e3ac9c25
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2022-08-14 19:46:59 +0200

    save token in npmrc

--- gulpfile.js/deploy.js
+++ gulpfile.js/deploy.js
@@ -25,7 +25,7 @@ async function publish() {
 	}
 	if (deployedVersion.prerelease[0] === 'rc' || currentVersion.prerelease[0] === 'rc') {
 		const npmToken = process.env.NPM_TOKEN;
-		await executeProcess('npm', [ 'config', 'set', `//registry.npmjs.org/:_authToken=${npmToken}`] );
+		await fse.writeFile('.npmrc', `//registry.npmjs.org/:_authToken=${npmToken}`);
 		await executeProcess('npm', [ 'pack' ]);
 		await executeProcess('npm', [ 'publish' ]);
 	} else {
